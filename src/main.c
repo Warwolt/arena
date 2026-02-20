a7325c849c0e4453aae0399c5be3d1e26c51f580
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-02-20 15:04:05 +0100

    spinning donut

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 
 #include "logging.h"
 
+#include <math.h>
+
 int main(void) {
 	initialize_logging();
 	SetTraceLogLevel(LOG_WARNING);
@@ -12,7 +14,7 @@ int main(void) {
 	const char* image_path = "resource/image/spinning_donut.png";
 	Image spinning_donut_image = LoadImage(image_path);
 	if (spinning_donut_image.data == NULL) {
-		LOG_ERROR("Couldn't load image \"%s\"!\n", image_path);
+		LOG_ERROR("Couldn't load image \"%s\"!", image_path);
 		goto exit_program;
 	}
 	Texture2D spinning_donut_texture = LoadTextureFromImage(spinning_donut_image);
@@ -21,11 +23,15 @@ int main(void) {
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		{
-			ClearBackground(BLACK);
+			ClearBackground(LIME);
+
+			const int time_now = GetTime() * 1000; // ms
+			const int frame_time = 70; // ms
+			const int num_frames = 12;
+			const int index = (time_now % (num_frames * frame_time)) / frame_time;
 
 			// Draw sprite sheet
 			const Vector2 sprite_size = { 64, 64 };
-			int index = 5;
 			Rectangle sprite_rect = { 64 * index, 64 * index, 64, 64 };
 			Vector2 sprite_pos = { (GetScreenWidth() - sprite_size.x) / 2, (GetScreenHeight() - sprite_size.y) / 2 };
 			DrawTextureRec(spinning_donut_texture, sprite_rect, sprite_pos, WHITE);
