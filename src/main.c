525836349c522f8652bd3aa3e7b611731237a254
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-02-20 19:25:56 +0100

    draw_sprite_centered

--- src/main.c
+++ src/main.c
@@ -6,6 +6,12 @@
 #include <math.h>
 #include <stdio.h>
 
+typedef struct Spritesheet {
+	Texture2D texture;
+	int sprite_width;
+	int sprite_height;
+} Spritesheet;
+
 Texture2D load_texture_from_file(const char* filename) {
 	Image image = LoadImage(filename);
 	if (image.data == NULL) {
@@ -16,14 +22,36 @@ Texture2D load_texture_from_file(const char* filename) {
 	return texture;
 }
 
+void draw_sprite_centered(const Spritesheet* spritesheet, int sprite_index, Vector2 position, Color tint) {
+	Rectangle rect = {
+		.x = spritesheet->sprite_width * sprite_index,
+		.y = spritesheet->sprite_height * sprite_index,
+		.width = spritesheet->sprite_width,
+		.height = spritesheet->sprite_height,
+	};
+	Vector2 centered_position = {
+		.x = position.x - rect.width / 2,
+		.y = position.y - rect.height / 2,
+	};
+	DrawTextureRec(spritesheet->texture, rect, centered_position, tint);
+}
+
 int main(void) {
 	initialize_logging();
 	SetTraceLogLevel(LOG_WARNING);
 	InitWindow(800, 450, "Program");
 	LOG_INFO("Created window");
 
-	Texture2D donut_texture = load_texture_from_file("resource/image/spinning_donut.png");
-	Texture2D coffee_texture = load_texture_from_file("resource/image/spinning_coffee.png");
+	Spritesheet donut_spritesheet = {
+		.texture = load_texture_from_file("resource/image/spinning_donut.png"),
+		.sprite_width = 64,
+		.sprite_height = 64,
+	};
+	Spritesheet coffee_spritesheet = {
+		.texture = load_texture_from_file("resource/image/spinning_coffee.png"),
+		.sprite_width = 64,
+		.sprite_height = 64,
+	};
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
@@ -31,31 +59,26 @@ int main(void) {
 			ClearBackground(LIME);
 
 			const int time_now = GetTime() * 1000; // ms
-			const int frame_time = 80; // ms
+			const int frame_time = 70; // ms
 			const int num_frames = 12;
 			const int index = (time_now % (num_frames * frame_time)) / frame_time;
-			const Vector2 sprite_size = { 64, 64 };
 
 			// Draw donut
 			{
-				Rectangle sprite_rect = { 64 * index, 64 * index, 64, 64 };
-				Vector2 sprite_pos = {
-					(GetScreenWidth() - sprite_size.x) / 2 - 48,
-					(GetScreenHeight() - sprite_size.y) / 2,
+				Vector2 position = {
+					GetScreenWidth() / 2 - 48,
+					GetScreenHeight() / 2,
 				};
-				DrawTextureRec(donut_texture, sprite_rect, Vector2Add(sprite_pos, (Vector2) { -4, 8 }), (Color) { 0, 0, 0, 16 });
-				DrawTextureRec(donut_texture, sprite_rect, sprite_pos, WHITE);
+				draw_sprite_centered(&donut_spritesheet, index, position, WHITE);
 			}
 
 			// Draw coffee
 			{
-				Rectangle sprite_rect = { 64 * index, 64 * index, 64, 64 };
-				Vector2 sprite_pos = {
-					(GetScreenWidth() - sprite_size.x) / 2 + 48,
-					(GetScreenHeight() - sprite_size.y) / 2,
+				Vector2 position = {
+					GetScreenWidth() / 2 + 48,
+					GetScreenHeight() / 2,
 				};
-				DrawTextureRec(coffee_texture, sprite_rect, Vector2Add(sprite_pos, (Vector2) { -4, 8 }), (Color) { 0, 0, 0, 16 });
-				DrawTextureRec(coffee_texture, sprite_rect, sprite_pos, WHITE);
+				draw_sprite_centered(&coffee_spritesheet, index, position, WHITE);
 			}
 
 			// Draw FPS
