f47641762e6d354b34ac4ba8093ee49f5f357505
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-02-20 19:29:54 +0100

    Two animated images (#7)
    add Spritesheet type 
    load_texture_from_file
    draw_sprite_centered
    add fps debug overlay code
    Render both donut and coffee animation

--- src/main.c
+++ src/main.c
@@ -4,23 +4,58 @@
 #include "logging.h"
 
 #include <math.h>
+#include <stdio.h>
+
+typedef struct Spritesheet {
+	Texture2D texture;
+	int sprite_width;
+	int sprite_height;
+} Spritesheet;
+
+Texture2D load_texture_from_file(const char* filename) {
+	Image image = LoadImage(filename);
+	if (image.data == NULL) {
+		return (Texture2D) { 0 };
+	}
+	Texture2D texture = LoadTextureFromImage(image);
+	UnloadImage(image);
+	return texture;
+}
+
+void draw_sprite_centered(const Spritesheet* spritesheet, int sprite_index, Vector2 position, Color tint) {
+	Rectangle rect = {
+		.x = spritesheet->sprite_width * sprite_index,
+		.y = spritesheet->sprite_height * sprite_index,
+		.width = spritesheet->sprite_width,
+		.height = spritesheet->sprite_height,
+	};
+	Vector2 centered_position = {
+		.x = position.x - rect.width / 2,
+		.y = position.y - rect.height / 2,
+	};
+	DrawTextureRec(spritesheet->texture, rect, centered_position, tint);
+}
 
 int main(void) {
+	/* Init */
 	initialize_logging();
 	SetTraceLogLevel(LOG_WARNING);
 	InitWindow(800, 450, "Program");
 	LOG_INFO("Created window");
 
-	// Load sprite sheet
-	const char* image_path = "resource/image/spinning_donut.png";
-	Image spinning_donut_image = LoadImage(image_path);
-	if (spinning_donut_image.data == NULL) {
-		LOG_ERROR("Couldn't load image \"%s\"!", image_path);
-		goto exit_program;
-	}
-	Texture2D spinning_donut_texture = LoadTextureFromImage(spinning_donut_image);
-	UnloadImage(spinning_donut_image);
+	/* Load resources */
+	Spritesheet donut_spritesheet = {
+		.texture = load_texture_from_file("resource/image/spinning_donut.png"),
+		.sprite_width = 64,
+		.sprite_height = 64,
+	};
+	Spritesheet coffee_spritesheet = {
+		.texture = load_texture_from_file("resource/image/spinning_coffee.png"),
+		.sprite_width = 64,
+		.sprite_height = 64,
+	};
 
+	/* Run program */
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		{
@@ -31,17 +66,36 @@ int main(void) {
 			const int num_frames = 12;
 			const int index = (time_now % (num_frames * frame_time)) / frame_time;
 
-			// Draw sprite sheet
-			const Vector2 sprite_size = { 64, 64 };
-			Rectangle sprite_rect = { 64 * index, 64 * index, 64, 64 };
-			Vector2 sprite_pos = { (GetScreenWidth() - sprite_size.x) / 2, (GetScreenHeight() - sprite_size.y) / 2 };
-			DrawTextureRec(spinning_donut_texture, sprite_rect, Vector2Add(sprite_pos, (Vector2) { -8, 12 }), (Color) { 0, 0, 0, 16 });
-			DrawTextureRec(spinning_donut_texture, sprite_rect, sprite_pos, WHITE);
+			// Draw donut
+			{
+				Vector2 position = {
+					GetScreenWidth() / 2 - 48,
+					GetScreenHeight() / 2,
+				};
+				draw_sprite_centered(&donut_spritesheet, index, position, WHITE);
+			}
+
+			// Draw coffee
+			{
+				Vector2 position = {
+					GetScreenWidth() / 2 + 48,
+					GetScreenHeight() / 2,
+				};
+				draw_sprite_centered(&coffee_spritesheet, index, position, WHITE);
+			}
+
+			// Draw FPS
+			const bool draw_debug_overlay = false;
+			if (draw_debug_overlay) {
+				char text[128] = { 0 };
+				snprintf(text, 128, "FPS: %d", GetFPS());
+				DrawText(text, 0, 0, 16, WHITE);
+			}
 		}
 		EndDrawing();
 	}
 
-exit_program:
+	/* Shutdown */
 	CloseWindow();
 	return 0;
 }
