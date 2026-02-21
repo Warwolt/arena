d6edfc5f3cf0489bbbf4d65c1331a3edd3ca34a8
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-02-22 00:06:10 +0100

    remove

--- test/dict_tests.c
+++ test/dict_tests.c
@@ -2,15 +2,6 @@
 
 #include <stddef.h>
 
-// Dict_insert
-// Dict_remove
-// Dict_get
-// Dict_set
-
-// dense_idx < size
-// AND
-// dense[dense_idx].sparse_idx == idx
-
 #define MAX_TEST_ITEMS 64
 typedef struct TestDict {
 	size_t indices[MAX_TEST_ITEMS];
@@ -19,30 +10,82 @@ typedef struct TestDict {
 	size_t size;
 } TestDict;
 
-#define Dict_insert(dict, key, value) false
-#define Dict_get(dict, key, value) false
-
 bool TestDict_insert(TestDict* dict, int key, int value) {
 	/* Skip the zero-key */
 	if (key == 0) {
 		return false;
 	}
 
-	/* Check if key exists */
-	size_t index = dict->indices[key - 1];
-	const bool key_already_exists = dict->keys[index] == key;
-	if (key_already_exists) {
+	/* Check if key already exists */
+	const size_t maybe_index = dict->indices[key - 1];
+	const bool key_exists = dict->keys[maybe_index] == key;
+	if (key_exists) {
 		return false;
 	}
 
 	/* Insert new key */
+	const size_t index = dict->size;
+	dict->indices[key - 1] = index;
 	dict->keys[index] = key;
 	dict->values[index] = value;
+	dict->size += 1;
 	return true;
 }
 
+void TestDict_remove(TestDict* dict, int key) {
+	/* Skip the zero-key */
+	if (key == 0) {
+		return;
+	}
+
+	/* Check if key exists */
+	const size_t index = dict->indices[key - 1];
+	const bool key_exists = dict->keys[index] == key;
+	if (!key_exists) {
+		return;
+	}
+
+	/* Replace element to remove with last element */
+	const size_t last_index = dict->size - 1;
+	const int last_key = dict->keys[last_index];
+	dict->indices[key - 1] = dict->indices[last_key - 1];
+	dict->keys[index] = dict->keys[last_index];
+	dict->values[index] = dict->values[last_index];
+	dict->size -= 1;
+}
+
 bool TestDict_get(TestDict* dict, int key, int* value) {
-	*value = 1234;
+	/* Skip the zero-key */
+	if (key == 0) {
+		return false;
+	}
+
+	/* Check if key exists */
+	const size_t index = dict->indices[key - 1];
+	const bool key_exists = dict->keys[index] == key;
+	if (!key_exists) {
+		return false;
+	}
+
+	/* Return value */
+	*value = dict->values[index];
+	return true;
+}
+
+bool TestDict_set(TestDict* dict, int key, int value) {
+	/* Skip the zero-key */
+	if (key == 0) {
+		return false;
+	}
+
+	/* Update value if key exists */
+	const size_t index = dict->indices[key - 1];
+	const bool key_exists = dict->keys[index] == key;
+	if (!key_exists) {
+		return false;
+	}
+
+	dict->values[index] = value;
 	return true;
 }
 
@@ -50,27 +93,121 @@ TEST(DictTests, InsertElement_GetElement) {
 	TestDict dict = { 0 };
 	int key = 11;
 
-	bool did_insert = TestDict_insert(&dict, 11, 1234);
+	bool did_insert = TestDict_insert(&dict, key, 1234);
 	int value = 0;
-	bool did_get = TestDict_get(&dict, 11, &value);
+	bool did_get = TestDict_get(&dict, key, &value);
 
 	EXPECT_TRUE(did_insert);
 	EXPECT_TRUE(did_get);
 	EXPECT_EQ(value, 1234);
+	EXPECT_EQ((int)dict.size, 1);
+}
+
+TEST(DictTests, InsertMultipleElements_GetThoseElements) {
+	TestDict dict = { 0 };
+	int key1 = 1;
+	int key2 = 2;
+	int key3 = 3;
+
+	TestDict_insert(&dict, key1, 11);
+	TestDict_insert(&dict, key2, 22);
+	TestDict_insert(&dict, key3, 33);
+
+	int value1 = 0;
+	int value2 = 0;
+	int value3 = 0;
+	bool did_get1 = TestDict_get(&dict, key1, &value1);
+	bool did_get2 = TestDict_get(&dict, key2, &value2);
+	bool did_get3 = TestDict_get(&dict, key3, &value3);
+
+	EXPECT_TRUE(did_get1);
+	EXPECT_TRUE(did_get2);
+	EXPECT_TRUE(did_get3);
+	EXPECT_EQ(value1, 11);
+	EXPECT_EQ(value2, 22);
+	EXPECT_EQ(value3, 33);
+	EXPECT_EQ((int)dict.size, 3);
+}
+
+TEST(DictTests, InsertElement_ZeroKey_DoesNothing) {
+	TestDict dict = { 0 };
+	int zero_key = 0;
+
+	bool did_insert = TestDict_insert(&dict, zero_key, 1234);
+	int value = 0;
+	bool did_get = TestDict_get(&dict, zero_key, &value);
+
+	EXPECT_FALSE(did_insert);
+	EXPECT_FALSE(did_get);
+	EXPECT_EQ(value, 0);
+	EXPECT_EQ((int)dict.size, 0);
 }
 
 TEST(DictTests, InsertElement_ExistingKey_DoesNothing) {
 	TestDict dict = { 0 };
 	int key = 11;
 
-	TestDict_insert(&dict, 11, 1234);
-	bool did_insert = TestDict_insert(&dict, 11, 4567);
+	TestDict_insert(&dict, key, 12);
+	bool did_insert = TestDict_insert(&dict, key, 34);
 	int value = 0;
-	bool did_get = TestDict_get(&dict, 11, &value);
+	bool did_get = TestDict_get(&dict, key, &value);
 
 	EXPECT_FALSE(did_insert);
 	EXPECT_TRUE(did_get);
-	EXPECT_EQ(value, 1234);
+	EXPECT_EQ(value, 12);
+	EXPECT_EQ((int)dict.size, 1);
 }
 
-// InsertElement_ZeroKey_DoesNothing
+TEST(DictTests, SetElement_MissingKey_DoesNotUpdate) {
+	TestDict dict = { 0 };
+	int key = 1;
+
+	bool did_set = TestDict_set(&dict, key, 1234);
+	int value = 0;
+	bool did_get = TestDict_get(&dict, key, &value);
+
+	EXPECT_FALSE(did_set);
+	EXPECT_FALSE(did_get);
+	EXPECT_EQ(value, 0);
+}
+
+TEST(DictTests, SetElement_ExistingKey_UpdatesValue) {
+	TestDict dict = { 0 };
+	int key = 1;
+
+	TestDict_insert(&dict, key, 1234);
+	bool did_set = TestDict_set(&dict, key, 5678);
+	int value = 0;
+	bool did_get = TestDict_get(&dict, key, &value);
+
+	EXPECT_TRUE(did_set);
+	EXPECT_TRUE(did_get);
+	EXPECT_EQ(value, 5678);
+}
+
+TEST(DictTests, InsertElements_RemoveOneElement_CanStillGetRemaining) {
+	TestDict dict = { 0 };
+	int key1 = 1;
+	int key2 = 2;
+	int key3 = 3;
+
+	TestDict_insert(&dict, key1, 1001);
+	TestDict_insert(&dict, key2, 2002);
+	TestDict_insert(&dict, key3, 3003);
+	TestDict_remove(&dict, key2);
+
+	int value1 = 0;
+	int value2 = 0;
+	int value3 = 0;
+	bool did_get1 = TestDict_get(&dict, key1, &value1);
+	bool did_get2 = TestDict_get(&dict, key2, &value2);
+	bool did_get3 = TestDict_get(&dict, key3, &value3);
+
+	EXPECT_TRUE(did_get1);
+	EXPECT_FALSE(did_get2);
+	EXPECT_TRUE(did_get3);
+	EXPECT_EQ(value1, 1001);
+	EXPECT_EQ(value2, 0);
+	EXPECT_EQ(value3, 3003);
+	EXPECT_EQ((int)dict.size, 2);
+}
