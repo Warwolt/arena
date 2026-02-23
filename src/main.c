f3bfd9d926f43bf395c8f16b67c2f601b740df77
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-02-24 00:38:48 +0100

    raylib_extra

--- src/main.c
+++ src/main.c
@@ -5,9 +5,9 @@
 #include "resource.h"
 #include "win32.h"
 
+#include "raylib_extra.h"
 #include <raylib.h>
 #include <raymath.h>
-#include <rlgl.h>
 
 #include <math.h>
 #include <stdint.h>
@@ -18,93 +18,6 @@
 // UFO 50 is 16:9 at 384x216 resolution
 #define RESOLUTION_WIDTH (int)768
 #define RESOLUTION_HEIGHT (int)432
-
-void DrawTextureRecSheared(Texture2D texture, Rectangle source, Rectangle dest, float angle, Color tint) {
-	if (texture.id <= 0) {
-		return;
-	}
-
-	Vector2 position = (Vector2) { dest.x, dest.y };
-	float width = (float)texture.width;
-	float height = (float)texture.height;
-
-	bool flipX = false;
-
-	if (source.width < 0) {
-		flipX = true;
-		source.width *= -1;
-	}
-	if (source.height < 0)
-		source.y -= source.height;
-
-	if (dest.width < 0)
-		dest.width *= -1;
-	if (dest.height < 0)
-		dest.height *= -1;
-
-	float tanAngle = tanf(DEG2RAD * angle);
-
-	// coordinates relative to top left of quad
-	Vector2 localTopLeft = { 0.0f, 0.0f };
-	Vector2 localTopRight = { dest.width, 0.0f };
-	Vector2 localBottomLeft = { 0.0f, dest.height };
-	Vector2 localBottomRight = { dest.width, dest.height };
-
-	// world space coordinates of sheared quad
-	Vector2 shearedTopLeft = {
-		position.x + localTopLeft.x + tanAngle * localTopLeft.y,
-		position.y + localTopLeft.y,
-	};
-	Vector2 shearedTopRight = {
-		position.x + localTopRight.x + tanAngle * localTopRight.y,
-		position.y + localTopRight.y,
-	};
-	Vector2 shearedBottomLeft = {
-		position.x + localBottomLeft.x + tanAngle * localBottomLeft.y,
-		position.y + localBottomLeft.y,
-	};
-	Vector2 shearedBottomRight = {
-		position.x + localBottomRight.x + tanAngle * localBottomRight.y,
-		position.y + localBottomRight.y,
-	};
-
-	rlSetTexture(texture.id);
-	rlBegin(RL_QUADS);
-
-	rlColor4ub(tint.r, tint.g, tint.b, tint.a);
-	rlNormal3f(0.0f, 0.0f, 1.0f); // Normal vector pointing towards viewer
-
-	// Top-left corner for texture and quad
-	if (flipX)
-		rlTexCoord2f((source.x + source.width) / width, source.y / height);
-	else
-		rlTexCoord2f(source.x / width, source.y / height);
-	rlVertex2f(shearedTopLeft.x, shearedTopLeft.y);
-
-	// Bottom-left corner for texture and quad
-	if (flipX)
-		rlTexCoord2f((source.x + source.width) / width, (source.y + source.height) / height);
-	else
-		rlTexCoord2f(source.x / width, (source.y + source.height) / height);
-	rlVertex2f(shearedBottomLeft.x, shearedBottomLeft.y);
-
-	// Bottom-right corner for texture and quad
-	if (flipX)
-		rlTexCoord2f(source.x / width, (source.y + source.height) / height);
-	else
-		rlTexCoord2f((source.x + source.width) / width, (source.y + source.height) / height);
-	rlVertex2f(shearedBottomRight.x, shearedBottomRight.y);
-
-	// Top-right corner for texture and quad
-	if (flipX)
-		rlTexCoord2f(source.x / width, source.y / height);
-	else
-		rlTexCoord2f((source.x + source.width) / width, source.y / height);
-	rlVertex2f(shearedTopRight.x, shearedTopRight.y);
-
-	rlEnd();
-	rlSetTexture(0);
-}
 
 int compare_position_ids_by_y_coordinate(void* ctx, const void* lhs, const void* rhs) {
 	EntityManager* entities = (EntityManager*)ctx;
