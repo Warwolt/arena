a424ca57486013ed56d4e434efacfb5e6284690c
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-03-10 20:13:00 +0100

    remaining tests

--- test/array_map_tests.c
+++ test/array_map_tests.c
@@ -47,18 +47,28 @@ bool TestArrayMap_insert(TestArrayMap* map, const char* key, int value) {
 		return false;
 	}
 
-	/* If key exists, update value*/
+	/* Update value if key already exists */
 	const int maybe_index = TestArrayMap_key_index(map, key);
 	if (maybe_index != ARRAY_MAP_MISSING_KEY) {
 		map->values[maybe_index] = value;
 		return true;
 	}
 
-	/* Push new value */
+	/* Else, push new key-value pair */
 	const size_t index = map->num_values++;
 	strncpy_s(map->keys[index], ARRAY_MAP_KEY_LENGTH, key, _TRUNCATE);
 	map->values[index] = value;
 	return true;
+}
+
+void TestArrayMap_remove(TestArrayMap* map, const char* key) {
+	const int maybe_index = TestArrayMap_key_index(map, key);
+	if (maybe_index != ARRAY_MAP_MISSING_KEY) {
+		// Replace item to remove with last
+		map->values[maybe_index] = map->values[map->num_values - 1];
+		strncpy_s(map->keys[maybe_index], ARRAY_MAP_KEY_LENGTH, map->keys[map->num_values - 1], _TRUNCATE);
+		map->num_values--;
+	}
 }
 
 bool TestArrayMap_get(TestArrayMap* map, const char* key, int* value) {
@@ -67,6 +77,15 @@ bool TestArrayMap_get(TestArrayMap* map, const char* key, int* value) {
 		return false;
 	}
 	*value = map->values[maybe_index];
+	return true;
+}
+
+bool TestArrayMap_get_ptr(TestArrayMap* map, const char* key, int** value_ptr) {
+	const int maybe_index = TestArrayMap_key_index(map, key);
+	if (maybe_index == ARRAY_MAP_MISSING_KEY) {
+		return false;
+	}
+	*value_ptr = &map->values[maybe_index];
 	return true;
 }
 
@@ -136,4 +155,83 @@ TEST(ArrayMapTests, InsertElement_EmptyKey_DoesNothing) {
 	EXPECT_FALSE(contains_zero);
 	EXPECT_EQ(value, 0);
 	EXPECT_EQ((int)map.num_values, 0);
+}
+
+TEST(ArrayMapTests, InsertElement_ExistingKey_UpdatesValue) {
+	TestArrayMap array = { 0 };
+	const char* key = "bar";
+
+	TestArrayMap_insert(&array, key, 12);
+	bool did_insert = TestArrayMap_insert(&array, key, 34);
+	int value = 0;
+	bool did_get = TestArrayMap_get(&array, key, &value);
+
+	EXPECT_TRUE(did_insert);
+	EXPECT_TRUE(did_get);
+	EXPECT_EQ(value, 34);
+	EXPECT_EQ((int)array.num_values, 1);
+}
+
+TEST(ArrayMapTests, InsertElements_RemoveElement) {
+	TestArrayMap array = { 0 };
+	const char* key = "Alice";
+
+	TestArrayMap_insert(&array, key, 1001);
+	TestArrayMap_remove(&array, key);
+
+	int value = 0;
+	bool did_get = TestArrayMap_get(&array, key, &value);
+	bool contains_element = TestArrayMap_contains(&array, key);
+
+	EXPECT_FALSE(did_get);
+	EXPECT_FALSE(contains_element);
+	EXPECT_EQ(value, 0);
+	EXPECT_EQ((int)array.num_values, 0);
+}
+
+TEST(ArrayMapTests, InsertMultipleElements_RemoveOneElement_GetRemaining) {
+	TestArrayMap array = { 0 };
+	const char* key1 = "1";
+	const char* key2 = "2";
+	const char* key3 = "3";
+
+	TestArrayMap_insert(&array, key1, 10);
+	TestArrayMap_insert(&array, key2, 20);
+	TestArrayMap_insert(&array, key3, 30);
+	TestArrayMap_remove(&array, key2);
+
+	int value1 = 0;
+	int value2 = 0;
+	int value3 = 0;
+	bool did_get1 = TestArrayMap_get(&array, key1, &value1);
+	bool did_get2 = TestArrayMap_get(&array, key2, &value2);
+	bool did_get3 = TestArrayMap_get(&array, key3, &value3);
+	bool contains_element1 = TestArrayMap_contains(&array, key1);
+	bool contains_element2 = TestArrayMap_contains(&array, key2);
+	bool contains_element3 = TestArrayMap_contains(&array, key3);
+
+	EXPECT_TRUE(did_get1);
+	EXPECT_FALSE(did_get2);
+	EXPECT_TRUE(did_get3);
+	EXPECT_TRUE(contains_element1);
+	EXPECT_FALSE(contains_element2);
+	EXPECT_TRUE(contains_element3);
+	EXPECT_EQ(value1, 10);
+	EXPECT_EQ(value2, 0);
+	EXPECT_EQ(value3, 30);
+	EXPECT_EQ((int)array.num_values, 2);
+}
+
+TEST(ArrayMapTests, InsertElement_GetElementPointer) {
+	TestArrayMap array = { 0 };
+	const char* key = "Bob";
+
+	TestArrayMap_insert(&array, key, 1234);
+	int* value_ptr = 0;
+	TestArrayMap_get_ptr(&array, key, &value_ptr);
+	*value_ptr = 4567;
+	int value = 0;
+	TestArrayMap_get(&array, key, &value);
+
+	EXPECT_EQ(value, 4567);
 }
