b4010b67c438945fabf6b9c374d76367d523ddfa
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-03-13 15:46:11 +0100

    Add asserts, make asserts and logging unit test friendly (#33)

---
+++ src/platform/assert.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "platform/logging.h"
+
+#include <stdio.h>
+
+#define ASSERT(condition, ...)                                               \
+	if (!(condition)) {                                                      \
+		char msg[512];                                                       \
+		int offset = sprintf_s(msg, 512, "ASSERT(%s) failed: ", #condition); \
+		sprintf_s(msg + offset, 512 - offset, __VA_ARGS__);                  \
+		LOG_FATAL("%s", msg);                                                \
+		debug_exit(msg);                                                     \
+	}
+
+void enable_unit_test_debug_exit(void);
+void debug_exit(const char* message);
