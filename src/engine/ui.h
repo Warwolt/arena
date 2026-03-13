317008c156fb5f7cd5aedaccfc5241ab6ed28fd3
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-03-13 16:04:42 +0100

    UI System

---
+++ src/engine/ui.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <stdbool.h>
+
+#define UIMenu_MaxMenus 4
+#define UIMenu_MaxMenuItems 16
+#define UIMenu_MaxLabelLength 256
+
+typedef struct UIMenuItem {
+	char label[UIMenu_MaxLabelLength];
+	bool is_focused;
+} UIMenuItem;
+
+typedef struct UIMenu {
+	UIMenuItem items[UIMenu_MaxMenuItems];
+	char label[UIMenu_MaxLabelLength];
+	int num_items;
+} UIMenu;
+
+typedef struct UIView {
+	UIMenu menus[UIMenu_MaxMenus];
+	int num_menus;
+} UIView;
+
+typedef struct UIInput {
+	bool down_pressed;
+	bool up_pressed;
+	bool select_pressed;
+} UIInput;
+
+void UI_initialize(void);
+const UIView* UI_view(void);
+
+void UI_begin(UIInput input);
+void UI_end(void);
+
+void UI_reset_next_keyboard_focus(void);
+
+void UI_menu_begin(const char* label);
+void UI_menu_end(void);
+bool UI_menu_item(const char* label);
