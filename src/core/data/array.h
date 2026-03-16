edb92f6f40e2157dfa011bdfcd16742ab7f9a738
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-03-16 10:46:51 +0100

    add todo

--- src/core/data/array.h
+++ src/core/data/array.h
@@ -21,8 +21,14 @@
 		size_t capacity;       \
 	}
 
-// #define Array_initialize
-#define Array_push(array, value)
+#define Array_initialize(array)                                                   \
+	do {                                                                          \
+		(array)->num_values = 0;                                                  \
+		(array)->capacity = sizeof((array)->values) / sizeof((array)->values[0]); \
+	} while (0)
+
+#define Array_push(array, value) \
+	Array_push_impl(sizeof((array)->values[0]), (char*)(array)->values, &(array)->num_values, (array)->capacity, (char*)value)
 #define Array_pop(array)
 
 bool Array_push_impl(size_t elem_size, char* array_values, size_t* array_num_values, size_t array_capacity, char* value);
