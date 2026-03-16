4d33a06439b59019c5a261dd26820590ac4fcbe1
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-03-16 23:38:44 +0100

    doubt

--- src/game/scene/debug/collision_debug.h
+++ src/game/scene/debug/collision_debug.h
@@ -18,6 +18,19 @@ typedef struct CollisionDebugScene {
 	int page;
 	int mouse_grabed_shape;
 
+	// FIXME:
+	// Is this really any better than the previous style container?
+	// "ShapeArray" doesn't include what length the array has, which reads awkward.
+	//
+	// It'd be great if we could write something like ShapeArray(64) and still
+	// magically have all the methods work, but that way we lose access to the
+	// ARRAY_CAPACITY and at that point why even bother with this at all?
+	//
+	// Is all of this a little too cute? Maybe this isn't worth bothering with?
+	// The following reads so incredibly clear:
+	// 		Shape shapes[64];
+	// 		size_t num_shapes;
+	//
 	ShapeArray shapes;
 	bool shape_is_overlapping[DEBUG_SHAPES_MAX];
 } CollisionDebugScene;
